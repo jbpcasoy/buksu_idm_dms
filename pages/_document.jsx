31374dd3f47081d9ccf96dd36cfa54dc82f48e95
Mark Zhang <68852130+archaga@users.noreply.github.com>
2023-02-14 11:59:32 +0800

    completed header & tables

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,8 +2,13 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='en'>
-      <Head />
+    <Html lang="en">
+      <Head>
+        <link
+          href="/your-path-to-uicons/css/uicons-[your-style].css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- views/layout/Header.jsx
+++ views/layout/Header.jsx
@@ -1,5 +1,4 @@
 import { signIn, signOut } from "next-auth/react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import LoginButton from "../LoginButton";
 
@@ -7,30 +6,46 @@ export default function Header() {
   const router = useRouter();
 
   return (
-    <nav className='flex items-center justify-between flex-wrap bg-gray-800 p-6'>
-      <ul className='flex'>
-        <li className='mr-6'>
-          <Link
-            className={`text-white hover:text-gray-400 ${
-              router.asPath === "/" ? "underline" : ""
-            }`}
-            href='/'>
-            Home
-          </Link>
-        </li>
-        <li className='mr-6'>
-          <Link
-            className={`text-white hover:text-gray-400 ${
-              router.asPath === "/me" ? "underline" : ""
-            }`}
-            href='/me'>
-            Profile
-          </Link>
-        </li>
-        <li className='mr-6'>
-          <LoginButton onSignIn={signIn} onSignOut={signOut} />
-        </li>
-      </ul>
-    </nav>
+    <header class="flex items-center justify-between bg-gray-800 pl-4 pr-6">
+      <a href="/">
+        <img src="/IMAGES/BukSUIMD.png" alt="Logo" className="w-64"></img>
+      </a>
+      <nav class="flex items-center">
+        <a href="/" title="Search" class="text-white mr-4 hover:text-gray-400">
+          <i class="fi fi-br-search text-white text-lg"></i>
+        </a>
+        <a href="/me" title="User" class="text-white mr-4 hover:text-gray-400">
+          <i class="fi fi-sr-user  text-white text-lg"></i>
+        </a>
+
+        <LoginButton onSignIn={signIn} onSignOut={signOut} />
+      </nav>
+    </header>
+    // <nav className='flex items-center justify-between flex-wrap bg-gray-800 p-4'>
+    //   <ul className='flex'>
+    //   <img src="/IMAGES/BukSUIMD.png" alt="Logo" className="w-64"></img>
+    //     <li className='mr-6'>
+    //       <Link
+    //         className={`text-white hover:text-gray-400 ${
+    //           router.asPath === "/" ? "underline" : ""
+    //         }`}
+    //         href='/'>
+    //         Home
+    //       </Link>
+    //     </li>
+    //     <li className='mr-6'>
+    //       <Link
+    //         className={`text-white hover:text-gray-400 ${
+    //           router.asPath === "/me" ? "underline" : ""
+    //         }`}
+    //         href='/me'>
+    //         Profile
+    //       </Link>
+    //     </li>
+    //     <li className='mr-6'>
+    //       <LoginButton onSignIn={signIn} onSignOut={signOut} />
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 }
