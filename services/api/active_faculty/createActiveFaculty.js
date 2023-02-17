72f7c4a99031bef707ec44954312412c8db58bc6
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-02-17 15:32:57 +0800

    implemented ActiveFaculty Department reference

--- services/api/active_faculty/createActiveFaculty.js
+++ services/api/active_faculty/createActiveFaculty.js
@@ -1,17 +1,43 @@
 import { PrismaClient } from "@prisma/client";
 
-export default async function createActiveFaculty({ userId, facultyId }) {
+export default async function createActiveFaculty({
+  userId,
+  facultyId,
+  departmentId,
+}) {
   const prisma = new PrismaClient();
 
   try {
+    const faculty = await findFaculty({
+      facultyId,
+      departmentId,
+    });
     const activeFaculty = await prisma.activeFaculty.create({
       data: {
-        facultyId,
+        departmentId,
+        facultyId: faculty.id,
         userId,
       },
     });
 
     return activeFaculty;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function findFaculty({ facultyId, departmentId }) {
+  const prisma = new PrismaClient();
+
+  try {
+    const faculty = await prisma.faculty.findFirstOrThrow({
+      where: {
+        departmentId: departmentId,
+        id: facultyId,
+      },
+    });
+
+    return faculty;
   } catch (error) {
     throw error;
   }
