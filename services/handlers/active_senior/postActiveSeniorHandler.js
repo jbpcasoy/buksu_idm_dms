6688ad1847eb63b3fdb58bb21bdbb1c61400abd4
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-02-17 15:05:33 +0800

    implemented SeniorId department reference

--- services/handlers/active_senior/postActiveSeniorHandler.js
+++ services/handlers/active_senior/postActiveSeniorHandler.js
@@ -1,10 +1,11 @@
 import createActiveSenior from "@/services/api/active_senior/createActiveSenior";
 
 export default async function postActiveSeniorHandler(req, res) {
-  const { seniorId } = req.body;
+  const { seniorId, departmentId } = req.body;
 
   const activeSenior = await createActiveSenior({
     seniorId,
+    departmentId,
   });
   return res.status(201).json(activeSenior);
 }
