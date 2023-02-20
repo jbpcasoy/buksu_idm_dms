191a798b29983b6570361bfb1adedc2f9c291955
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-02-20 11:40:12 +0800

    implemented custom mui theme

---
+++ theme/index.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#152033",
+    },
+    secondary: {
+      main: "#F2C050",
+    },
+  },
+});
