cdd7360de079d2d8e3d2799be766bf89bfbfd76a
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-02-20 11:11:00 +0800

    implemented admin user

---
+++ pages/admin/index.jsx
@@ -0,0 +1,10 @@
+import AdminLayout from "@/views/admin/layout/AdminLayout";
+import { Typography } from "@mui/material";
+
+export default function AdminHome() {
+  return (
+    <AdminLayout>
+      <Typography>AdminHome</Typography>
+    </AdminLayout>
+  );
+}
