17a21566ad5ecd68e3b7b1726f3d761b3ac9b9f5
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-02-26 10:05:10 +0800

    switched to sqlite db

--- prisma/migrations/20230212083042_im_original_filename/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `originalFileName` to the `IM` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "IM" ADD COLUMN     "originalFileName" TEXT NOT NULL;
