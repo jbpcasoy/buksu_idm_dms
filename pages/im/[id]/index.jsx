fb498b3a0876166d5686ef9f91d88870ba1712e7
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-02-28 16:12:22 +0800

    changed to only use google viewer on production

--- pages/im/[id]/index.jsx
+++ pages/im/[id]/index.jsx
@@ -45,10 +45,18 @@ export default function ViewIM() {
             </div>
           </div>
           {/* TODO change pdf url into dynamic */}
-          <iframe
-            src={`https://docs.google.com/gview?url=${process.env.NEXT_PUBLIC_HOST_URL}/api/download/file/${iM?.ActiveFile?.File.fileName}&embedded=true`}
-            className='w-full h-screen'
-          />
+          {process.env.NODE_ENV === "production" && (
+            <iframe
+              src={`https://docs.google.com/gview?url=${process.env.NEXT_PUBLIC_HOST_URL}/api/download/file/${iM?.ActiveFile?.File.fileName}&embedded=true`}
+              className='w-full h-screen'
+            />
+          )}
+          {process.env.NODE_ENV !== "production" && (
+            <iframe
+              src={`${process.env.NEXT_PUBLIC_HOST_URL}/api/download/file/${iM?.ActiveFile?.File.fileName}`}
+              className='w-full h-screen'
+            />
+          )}
         </div>
       </WithSidebar>
     </Layout>
