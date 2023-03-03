4bffc31f2be8a27f7a9f037386ce17b725661978
Mark Zhang <68852130+archaga@users.noreply.github.com>
2023-03-03 15:33:44 +0800

    updating im review form and responsiveness

--- pages/im/[id]/versions/[version_id]/index.jsx
+++ pages/im/[id]/versions/[version_id]/index.jsx
@@ -1,25 +1,42 @@
 import Layout from "@/components/layout/Layout";
+import frontendReadIM from "@/services/frontend/im/frontendReadIM";
+import IM from "@/views/im/IM";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function IMVersion() {
+  const router = useRouter();
+  const [iM, setIM] = useState();
+
+  useEffect(() => {
+    const id = router?.query?.id;
+    if (!id) return;
+
+    frontendReadIM(id).then((res) => {
+      setIM(res);
+    });
+  }, [router?.query?.id]);
+
+  console.log({ router });
   return (
     <Layout>
-      <div className="bg-white rounded-md p-4">
-        <div className="flex items-center justify-between mb-4">
-          <h2 className="text-lg font-medium">Document.pdf</h2>
-          <h2 className="text-xs uppercase font-medium">
+      <div className='bg-white rounded-md p-4'>
+        <div className='flex items-center justify-between mb-4'>
+          <h2 className='text-lg font-medium'>{IM?.title}</h2>
+          {/* <h2 className="text-xs uppercase font-medium">
             Version 1677046830191
-          </h2>
-          <div className="items-left">
+          </h2> */}
+          <div className='items-left'>
             <Link
-              href="/im/view"
-              className="text-CITLDarkBlue border border-CITLGray-main p-2 rounded hover:bg-CITLOrange"
+              href={`/im/${iM?.id}/versions`}
+              className='text-CITLDarkBlue border border-CITLGray-main p-2 rounded hover:bg-CITLOrange'
             >
               Current Version
             </Link>{" "}
             <Link
-              href="/"
-              className="text-CITLDarkBlue border border-CITLGray-main p-2 rounded hover:bg-CITLOrange"
+              href='/'
+              className='text-CITLDarkBlue border border-CITLGray-main p-2 rounded hover:bg-CITLOrange'
             >
               Download
             </Link>{" "}
@@ -27,8 +44,8 @@ export default function IMVersion() {
         </div>
         {/* TODO change pdf url into dynamic */}
         <iframe
-          src="https://docs.google.com/gview?url=https://www.africau.edu/images/default/sample.pdf&embedded=true"
-          className="w-full h-screen"
+          src='https://docs.google.com/gview?url=https://www.africau.edu/images/default/sample.pdf&embedded=true'
+          className='w-full h-screen'
         />
       </div>
     </Layout>
--- pages/review/form/rubrics5.jsx
+++ pages/review/form/rubrics5.jsx
@@ -6,121 +6,46 @@ import Link from "next/link";
 export default function Title() {
   return (
     <Layout>
-      <div className="pt-12">
-        <div className="flex items-center border border-CITLGray-lighter  bg-CITLGray-light m-2 p-3 relative rounded-lg shadow-lg overflow-hidden">
-          <div className="p-5 w-full">
-            <div className="mx-4 p-4">
-              <div className="flex items-center">
-                <div className="flex items-center text-CITLOrange relative">
-                  <div className="rounded-full transition duration-500 ease-in-out h-4 w-4 border-2 border-CITLOrange bg-CITLOrange text-left">
-                    <div className="absolute top-0 mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      basic info
-                    </div>
+      <div className='sm:pt-12'>
+        <div className='flex items-center border border-CITLGray-lighter  bg-CITLGray-light m-2 p-3 relative rounded-lg shadow-lg overflow-hidden'>
+          <div className='w-full'>
+            <div className='p-4 '>
+              <div className='flex mt-10'>
+                <div className='w-full border-t-4 transition duration-500 ease-in-out border-CITLOrange'>
+                  <div className=' top-0 mt-2 text-xs font-medium uppercase text-CITLOrange'>
+                    step 10 of 10
                   </div>
                 </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-white relative">
-                  <div className="rounded-full transition duration-500 ease-in-out h-4 w-4 border-2 bg-CITLOrange border-CITLOrange">
-                    <div className="absolute top-0 text-left mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      instructions
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-CITLOrange relative">
-                  <div className="rounded-full text-left transition duration-500 ease-in-out h-4 w-4 border-2 border-CITLOrange bg-CITLOrange">
-                    <div className="absolute top-0  mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      The title
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-CITLGray-lighter relative">
-                  <div className="rounded-full text-left transition duration-500 ease-in-out h-4 w-4 border-2 bg-CITLOrange border-CITLOrange">
-                    <div className="absolute top-0  mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      The Preface
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-CITLGray-lighter relative">
-                  <div className="rounded-full text-left transition duration-500 ease-in-out h-4 w-4 border-2 bg-CITLOrange border-CITLOrange">
-                    <div className="absolute top-0  mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      The chapters
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-CITLGray-lighter relative">
-                  <div className="rounded-full text-left transition duration-500 ease-in-out h-4 w-4 border-2 bg-CITLOrange border-CITLOrange">
-                    <div className="absolute top-0  mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      The learning outcomes
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-CITLGray-lighter relative">
-                  <div className="rounded-full text-left transition duration-500 ease-in-out h-4 w-4 border-2 bg-CITLOrange border-CITLOrange">
-                    <div className="absolute top-0  mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      The Concepts
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-CITLGray-lighter relative">
-                  <div className="rounded-full text-left transition duration-500 ease-in-out h-4 w-4 border-2 bg-CITLOrange border-CITLOrange">
-                    <div className="absolute top-0  mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      The Examples
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-CITLGray-lighter relative">
-                  <div className="rounded-full text-left transition duration-500 ease-in-out h-4 w-4 border-2 bg-CITLOrange border-CITLOrange">
-                    <div className="absolute top-0  mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      The Activities
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
-                <div className="flex items-center text-CITLGray-lighter relative">
-                  <div className="rounded-full text-left transition duration-500 ease-in-out h-4 w-4 border-2 bg-CITLOrange border-CITLOrange">
-                    <div className="absolute top-0  mt-6 w-32 text-xs font-medium uppercase text-CITLOrange">
-                      The Rubrics
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-CITLOrange"></div>
+                {/* <div className='w-3/12 border-t-4 ml-2 transition duration-500 ease-in-out border-CITLGray-lighter'></div> */}
               </div>
             </div>
 
-            <div className="px-6 pt-16 md:w-full ">
-              <h2 className="text-CITLDarkBlue font-bold text-2xl ">
+            <div className='px-2 pt-10 '>
+              <h2 className='text-CITLDarkBlue font-bold text-2xl '>
                 Instructional Material Review Form
               </h2>
-              <p className="mb-8 text-sm">Implementation Phase</p>
+              <p className='mb-8 text-sm'>Implementation Phase</p>
 
               <Section
                 questions={[
                   "They are prepared within the capability of the students.",
                 ]}
-                title=" The Rubrics (if applicable)"
+                title=' The Rubrics (if applicable)'
               />
 
-              <div className="flex p-2 mt-4">
+              <div className='flex  mt-4'>
                 <Link
-                  href="/review/form/rubrics4"
-                  className="text-base  flex justify-center px-4 py-2 rounded font-bold cursor-pointer shadow-xl hover:border-CITLOrange hover:text-CITLOrange bg-gray-100 text-CITLGray-main border border-CITLGray-main"
+                  href='/review/form/rubrics4'
+                  className='text-base  flex justify-center px-4 py-2 rounded font-bold cursor-pointer shadow-xl hover:border-CITLOrange hover:text-CITLOrange bg-gray-100 text-CITLGray-main border border-CITLGray-main'
                 >
                   Previous
                 </Link>
-                <div className="flex-auto flex flex-row-reverse">
+                <div className='flex-auto flex flex-row-reverse'>
                   <Link
-                    href="/review/form/"
-                    className="text-base  ml-2  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer shadow-xl hover:bg-CITLOrange   bg-CITLDarkBlue text-CITLWhite"
+                    href='/review/form/'
+                    className='text-base  ml-2  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer shadow-xl hover:bg-CITLOrange   bg-CITLDarkBlue text-CITLWhite'
                   >
-                    Next
+                    Submit
                   </Link>
                 </div>
               </div>
