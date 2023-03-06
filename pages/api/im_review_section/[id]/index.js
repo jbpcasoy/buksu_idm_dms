0e9d1660789cba03427b6def62bda7b1e08920d0
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-06 10:15:06 +0800

    implemented DELETE IMReviewSection

--- pages/api/im_review_section/[id]/index.js
+++ pages/api/im_review_section/[id]/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import deleteImReviewSectionHandler from "@/services/handlers/im_review_section/deleteImReviewSectionHandler";
 import getImReviewSectionHandler from "@/services/handlers/im_review_section/getImReviewSectionHandler";
 import putImReviewSectionHandler from "@/services/handlers/im_review_section/putImReviewSectionHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
@@ -11,6 +12,8 @@ export default async function handler(req, res) {
       return getImReviewSectionHandler(req, res);
     case "PUT":
       return putImReviewSectionHandler(req, res);
+    case "DELETE":
+      return deleteImReviewSectionHandler(req, res);
     default:
       methodNaHandler(req, res);
   }
