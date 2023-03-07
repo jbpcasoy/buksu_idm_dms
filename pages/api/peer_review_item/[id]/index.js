4acca07c3b656e62017ada01964bb42dda7bdca0
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-07 13:52:38 +0800

    implemented PUT PeerReviewItem

--- pages/api/peer_review_item/[id]/index.js
+++ pages/api/peer_review_item/[id]/index.js
@@ -1,6 +1,7 @@
 import { reqLog } from "@/services/api/logger";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 import getPeerReviewItemHandler from "@/services/handlers/peer_review_item/getPeerReviewItemHandler";
+import putReviewItemHandler from "@/services/handlers/peer_review_item/putReviewItemHandler";
 
 export default async function handler(req, res) {
   reqLog(req, res);
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getPeerReviewItemHandler(req, res);
+    case "PUT":
+      return putReviewItemHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
