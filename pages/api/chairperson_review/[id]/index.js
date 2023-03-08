29963ec7c7dd59b562b960a6df9b02f8ffb0e754
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-08 15:14:01 +0800

    implemented DELETE ChairpersonReview

--- pages/api/chairperson_review/[id]/index.js
+++ pages/api/chairperson_review/[id]/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import deleteChairpersonReviewHandler from "@/services/handlers/chairperson_review/deleteChairpersonReviewHandler";
 import getChairpersonReviewHandler from "@/services/handlers/chairperson_review/getChairpersonReviewHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getChairpersonReviewHandler(req, res);
+    case "DELETE":
+      return deleteChairpersonReviewHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
