c2e07c15542d852f4b0825e622e442fb45c21cbc
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-09 10:44:53 +0800

    transformed peer review to have view components

--- pages/im/[id]/review/peer/index.jsx
+++ pages/im/[id]/review/peer/index.jsx
@@ -1,7 +1,7 @@
-import ReviewPage from "@/components/page/review/ReviewPage";
 import IMInfo from "@/components/review/IMInfo";
 import Instructions from "@/components/review/Instructions";
 import PeerQuestion from "@/components/review/PeerQuestion";
+import ReviewPage from "@/components/review/ReviewPage";
 import { countQuestions, sections } from "@/constants/questions";
 import useUser from "@/hooks/useUser";
 import frontendReadIM from "@/services/frontend/im/frontendReadIM";
@@ -142,49 +142,6 @@ const PeerReviewPage = () => {
     console.log({ peerReview });
   }, [peerReview]);
 
-  return (
-    // <Layout>
-    //   <div className='sm:pt-12'>
-    //     <div className='flex items-center border border-CITLGray-lighter bg-CITLGray-light m-2 p-3 relative rounded-lg shadow-lg overflow-hidden'>
-    //       <div className='w-full '>
-    //         {step < steps.length - 1 && (
-    //           <div className='p-4 '>
-    //             <div className='flex mt-10'>
-    //               <div className='w-9/12 border-t-4 transition duration-500 ease-in-out border-CITLOrange'>
-    //                 <div className=' top-0 mt-2 text-xs font-medium uppercase text-CITLOrange'>
-    //                   step {step + 1} of {steps.length}
-    //                 </div>
-    //               </div>
-    //               <div className='w-3/12 border-t-4 ml-2 transition duration-500 ease-in-out border-CITLGray-lighter'></div>
-    //             </div>
-    //           </div>
-    //         )}
-    //         {step === steps.length - 1 && (
-    //           <div className='p-4 '>
-    //             <div className='flex mt-10'>
-    //               <div className='w-full border-t-4 transition duration-500 ease-in-out border-CITLOrange'>
-    //                 <div className=' top-0 mt-2 text-xs font-medium uppercase text-CITLOrange'>
-    //                   step {step + 1} of {steps.length}
-    //                 </div>
-    //               </div>
-    //               {/* <div className='w-3/12 border-t-4 ml-2 transition duration-500 ease-in-out border-CITLGray-lighter'></div> */}
-    //             </div>
-    //           </div>
-    //         )}
-
-    //         <div className='px-2 pt-10'>
-    //           <h2 className='text-CITLDarkBlue font-bold text-2xl '>
-    //             Instructional Material Review Form
-    //           </h2>
-    //           <p className='mb-8 text-sm'>Implementation Phase</p>
-
-    //           {steps[step]}
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </Layout>
-    <ReviewPage step={step} steps={steps} />
-  );
+  return <ReviewPage step={step} steps={steps} />;
 };
 export default PeerReviewPage;
