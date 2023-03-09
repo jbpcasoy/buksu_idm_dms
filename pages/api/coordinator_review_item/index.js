dda48bebef784cda19aa5e9eba6c5520699b4c69
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-09 15:34:06 +0800

    implemented POST CoordinatorReviewItem

---
+++ pages/api/coordinator_review_item/index.js
@@ -0,0 +1,14 @@
+import { reqLog } from "@/services/api/logger";
+import postCoordinatorReviewItemHandler from "@/services/handlers/coordinator_review_item/postCoordinatorReviewItemHandler";
+import methodNaHandler from "@/services/handlers/methodNaHandler";
+
+export default async function handler(req, res) {
+  reqLog(req, res);
+
+  switch (req.method) {
+    case "POST":
+      return postCoordinatorReviewItemHandler(req, res);
+    default:
+      return methodNaHandler(req, res);
+  }
+}
