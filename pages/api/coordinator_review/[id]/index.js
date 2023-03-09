30bb591f7f82e179803fbc514e13a12ad53ba766
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-09 15:09:43 +0800

    implemented DELETE CoordinatorReview

--- pages/api/coordinator_review/[id]/index.js
+++ pages/api/coordinator_review/[id]/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import deleteCoordinatorReviewHandler from "@/services/handlers/coordinator_review/deleteCoordinatorReviewHandler";
 import getCoordinatorReviewHandler from "@/services/handlers/coordinator_review/getCoordinatorReviewHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getCoordinatorReviewHandler(req, res);
+    case "DELETE":
+      return deleteCoordinatorReviewHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
