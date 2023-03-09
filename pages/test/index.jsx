0307f9051296463fb22f8e1d5f7c5d2357c7494e
Mark Zhang <68852130+archaga@users.noreply.github.com>
2023-03-09 13:09:34 +0800

    CITL-F-004 finish

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,55 +1,33 @@
+import Layout from "@/components/layout/Layout";
+
+const canvas = document.getElementById("signatureCanvas");
+const signaturePad = new SignaturePad(canvas);
+const signatureData = signaturePad.toDataURL();
+
+canvas.addEventListener("touchstart", (e) => {
+  // Prevent scrolling on mobile devices
+  e.preventDefault();
+});
+
+canvas.addEventListener("touchmove", (e) => {
+  // Prevent scrolling on mobile devices
+  e.preventDefault();
+
+  // Capture the signature as the user draws on the canvas
+  const touch = e.touches[0];
+  const point = { x: touch.clientX, y: touch.clientY };
+  signaturePad.addPoint(point.x, point.y);
+});
+
 export default function Test() {
-  function addRow() {
-    var table = document.getElementById("input-table");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = '<input type="text" name="column1[]">';
-    cell2.innerHTML = '<input type="text" name="column2[]">';
-    cell3.innerHTML = '<input type="text" name="column3[]">';
-  }
   return (
-    <div>
-      {" "}
-      <head>
-        <title>Input Table with Add Row Button</title>
-        {/* <style>
-      table, th, td {
-        border: 1px solid black;
-        border-collapse: collapse;
-        padding: 5px;
-      }
-      th {
-        background-color: lightgray;
-      }
-    </style> */}
-      </head>
-      <body>
-        <table id='input-table'>
-          <thead>
-            <tr>
-              <th>Column 1</th>
-              <th>Column 2</th>
-              <th>Column 3</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <input type='text' name='column1[]' />
-              </td>
-              <td>
-                <input type='text' name='column2[]' />
-              </td>
-              <td>
-                <input type='text' name='column3[]' />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button onclick='addRow()'>Add Row</button>
-      </body>
-    </div>
+    <Layout>
+      <div class='relative'>
+        <canvas
+          id='signatureCanvas'
+          className ='w-full h-full bg-white border border-gray-300'
+        ></canvas>
+      </div>
+    </Layout>
   );
 }
