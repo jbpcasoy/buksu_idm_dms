1c206ec7901c0dc6f614c63bbcdaa50709ef1a66
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-09 16:30:12 +0800

    implemented GET SubmittedCoordinatorReviews

--- pages/api/submitted_coordinator_review/index.js
+++ pages/api/submitted_coordinator_review/index.js
@@ -1,5 +1,6 @@
 import { reqLog } from "@/services/api/logger";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
+import getSubmittedCoordinatorReviewsHandler from "@/services/handlers/submitted_coordinator_review/getSubmittedCoordinatorReviewsHandler";
 import postSubmittedCoordinatorReview from "@/services/handlers/submitted_coordinator_review/postSubmittedCoordinatorReview";
 
 export default async function handler(req, res) {
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       return postSubmittedCoordinatorReview(req, res);
+    case "GET":
+      return getSubmittedCoordinatorReviewsHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
