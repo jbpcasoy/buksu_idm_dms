aeb98a218d9a3325d669bdf638b630aa51100bcc
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-13 15:28:48 +0800

    implemented PUT PeerSuggestionItem

--- pages/api/peer_suggestion_item/[id]/index.js
+++ pages/api/peer_suggestion_item/[id]/index.js
@@ -1,6 +1,7 @@
 import { reqLog } from "@/services/api/logger";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 import getPeerSuggestionItemHandler from "@/services/handlers/peer_suggestion_item/getPeerSuggestionItemHandler";
+import putPeerSuggestionItemHandler from "@/services/handlers/peer_suggestion_item/putPeerSuggestionItemHandler";
 
 export default async function handler(req, res) {
   reqLog(req, res);
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getPeerSuggestionItemHandler(req, res);
+    case "PUT":
+      return putPeerSuggestionItemHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
