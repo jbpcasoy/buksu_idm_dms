9b5c72a5340c3caf4212a757792a757fcae88967
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-14 10:42:44 +0800

    implemented UPDATE CoordinatorSuggestionItem

--- pages/api/coordinator_suggestion_item/[id]/index.js
+++ pages/api/coordinator_suggestion_item/[id]/index.js
@@ -1,5 +1,6 @@
 import { reqLog } from "@/services/api/logger";
 import getCoordinatorSuggestionItemHandler from "@/services/handlers/coordinator_suggestion_item/getCoordinatorSuggestionItemHandler";
+import putCoordinatorSuggestionItemHandler from "@/services/handlers/coordinator_suggestion_item/putCoordinatorSuggestionItemHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 
 export default async function handler(req, res) {
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getCoordinatorSuggestionItemHandler(req, res);
+    case "PUT":
+      return putCoordinatorSuggestionItemHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
