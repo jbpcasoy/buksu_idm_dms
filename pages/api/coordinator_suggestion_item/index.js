1ee405be6935c87ce30d66b36996f82549eae1c5
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-14 10:29:22 +0800

    implemented GET CoordinatorSuggestionItems

--- pages/api/coordinator_suggestion_item/index.js
+++ pages/api/coordinator_suggestion_item/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import getCoordinatorSuggestionItemsHandler from "@/services/handlers/coordinator_suggestion_item/getCoordinatorSuggestionItemsHandler";
 import postCoordinatorSuggestionItemHandler from "@/services/handlers/coordinator_suggestion_item/postCoordinatorSuggestionItemHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       return postCoordinatorSuggestionItemHandler(req, res);
+    case "GET":
+      return getCoordinatorSuggestionItemsHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
