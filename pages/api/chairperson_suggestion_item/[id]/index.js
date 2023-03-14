0b43603394b1bd05b4fe5efe430a808bd8d1f0a9
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-14 08:56:32 +0800

    implemented DELETE ChairpersonSuggestionItem

--- pages/api/chairperson_suggestion_item/[id]/index.js
+++ pages/api/chairperson_suggestion_item/[id]/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import deleteChairpersonSuggestionItemHandler from "@/services/handlers/chairperson_suggestion_item/deleteChairpersonSuggestionItemHandler";
 import getChairpersonSuggestionItemHandler from "@/services/handlers/chairperson_suggestion_item/getChairpersonSuggestionItemHandler";
 import putChairpersonSuggestionItemHandler from "@/services/handlers/chairperson_suggestion_item/putChairpersonSuggestionItemHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
@@ -11,6 +12,8 @@ export default async function handler(req, res) {
       return getChairpersonSuggestionItemHandler(req, res);
     case "PUT":
       return putChairpersonSuggestionItemHandler(req, res);
+    case "DELETE":
+      return deleteChairpersonSuggestionItemHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
