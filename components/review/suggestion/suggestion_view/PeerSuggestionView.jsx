b9aa3614c04dd63331931f8710f5c1aaeb83407e
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-16 14:25:01 +0800

    separated peer suggestion view into components

--- components/review/suggestion/suggestion_view/PeerSuggestionView.jsx
+++ components/review/suggestion/suggestion_view/PeerSuggestionView.jsx
@@ -2,6 +2,7 @@ import usePeerSuggestion from "@/hooks/usePeerSuggestion";
 import usePeerSuggestionItems from "@/hooks/usePeerSuggestionItems";
 import useSubmittedPeerReview from "@/hooks/useSubmittedPeerReview";
 import PeerSuggestionItemView from "./PeerSuggestionItemView";
+import SuggestionView from "./SuggestionView";
 
 export default function PeerSuggestionView({ peerReview }) {
   const {
@@ -19,56 +20,14 @@ export default function PeerSuggestionView({ peerReview }) {
   } = usePeerSuggestionItems({ peerSuggestionId: peerSuggestion?.id });
 
   return (
-    <div>
-      <p>Peer Suggestions</p>
-      <table className='w-full divide-y divide-CITLGray-light mb-2'>
-        <thead className='bg-CITLGray-light'>
-          <tr>
-            <th
-              scope='col'
-              className=' px-5 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
-            >
-              suggestion
-            </th>
-
-            <th
-              scope='col'
-              className=' py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
-            >
-              Action Taken
-            </th>
-
-            <th
-              scope='col'
-              className=' py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
-            >
-              Page No.
-            </th>
-            <th
-              scope='col'
-              className=' py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
-            >
-              Remarks
-            </th>
-            <th
-              scope='col'
-              className=' py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
-            >
-              Actions
-            </th>
-          </tr>
-        </thead>
-
-        <tbody className='bg-white divide-gray-200 overflow-y-auto'>
-          {peerSuggestionItems.map((peerSuggestionItem) => (
-            <PeerSuggestionItemView
-              key={peerSuggestionItem.id}
-              peerSuggestionItem={peerSuggestionItem}
-              viewOnly
-            />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <SuggestionView title={"Peer Suggestions"}>
+      {peerSuggestionItems.map((peerSuggestionItem) => (
+        <PeerSuggestionItemView
+          key={peerSuggestionItem.id}
+          peerSuggestionItem={peerSuggestionItem}
+          viewOnly
+        />
+      ))}
+    </SuggestionView>
   );
 }
---
+++ components/review/suggestion/suggestion_view/SuggestionView.jsx
@@ -0,0 +1,49 @@
+export default function SuggestionView({ children, title }) {
+  return (
+    <div>
+      <p>{title}</p>
+      <table className='w-full divide-y divide-CITLGray-light mb-2'>
+        <thead className='bg-CITLGray-light'>
+          <tr>
+            <th
+              scope='col'
+              className=' px-5 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+            >
+              suggestion
+            </th>
+
+            <th
+              scope='col'
+              className=' py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+            >
+              Action Taken
+            </th>
+
+            <th
+              scope='col'
+              className=' py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+            >
+              Page No.
+            </th>
+            <th
+              scope='col'
+              className=' py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+            >
+              Remarks
+            </th>
+            <th
+              scope='col'
+              className=' py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+            >
+              Actions
+            </th>
+          </tr>
+        </thead>
+
+        <tbody className='bg-white divide-gray-200 overflow-y-auto'>
+          {children}
+        </tbody>
+      </table>
+    </div>
+  );
+}
