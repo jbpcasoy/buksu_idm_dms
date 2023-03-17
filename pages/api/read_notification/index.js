7735912f281d0ab13aae8df5679daac2d67e8c46
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-17 15:27:51 +0800

    implemented GET ReadNotifications

--- pages/api/read_notification/index.js
+++ pages/api/read_notification/index.js
@@ -1,5 +1,6 @@
 import { reqLog } from "@/services/api/logger";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
+import getReadNotificationHandler from "@/services/handlers/read_notification/getReadNotificationHandler";
 import postReadNotificationHandler from "@/services/handlers/read_notification/postReadNotificationHandler";
 
 export default async function handler(req, res) {
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       return postReadNotificationHandler(req, res);
+    case "GET":
+      return getReadNotificationHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
