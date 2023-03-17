8950beda48136e24c8dd6c3cefc6c56f2a9fbf76
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-17 09:38:42 +0800

    renamed fill-rule into fillRule

--- pages/college/[id]/department/[departmentId]/index.jsx
+++ pages/college/[id]/department/[departmentId]/index.jsx
@@ -129,7 +129,7 @@ export default function DepartmentPage() {
                         <path
                           fillRule='evenodd'
                           d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
-                          clip-rule='evenodd'
+                          clipRule='evenodd'
                         ></path>
                       </svg>
                       <button
@@ -155,7 +155,7 @@ export default function DepartmentPage() {
                         <path
                           fillRule='evenodd'
                           d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
-                          clip-rule='evenodd'
+                          clipRule='evenodd'
                         ></path>
                       </svg>
                       <span className='inline-flex items-center px-2 py-2.5 text-sm font-medium text-center text-CITLDarkBlue border-b-2 border-CITLOrange rounded-none'>
