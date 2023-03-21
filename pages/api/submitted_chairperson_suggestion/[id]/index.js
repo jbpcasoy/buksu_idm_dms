5901ec2921aac5e0337942bded152907dce1579a
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-21 16:59:25 +0800

    implemented DELETE SubmittedChairpersonSuggestion

--- pages/api/submitted_chairperson_suggestion/[id]/index.js
+++ pages/api/submitted_chairperson_suggestion/[id]/index.js
@@ -1,5 +1,6 @@
 import { reqLog } from "@/services/api/logger";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
+import deleteSubmittedChairpersonSuggestionHandler from "@/services/handlers/submitted_chairperson_suggestion/deleteSubmittedChairpersonSuggestionHandler";
 import getSubmittedChairpersonSuggestionHandler from "@/services/handlers/submitted_chairperson_suggestion/getSubmittedChairpersonSuggestionHandler";
 
 export default async function handler(req, res) {
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getSubmittedChairpersonSuggestionHandler(req, res);
+    case "DELETE":
+      return deleteSubmittedChairpersonSuggestionHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
