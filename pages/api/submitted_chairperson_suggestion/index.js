d5b7c811e34e8b0c64ff8033084fb7a959667f9f
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-21 16:48:48 +0800

    implemented READ SubmittedChairpersonSuggestion

--- pages/api/submitted_chairperson_suggestion/index.js
+++ pages/api/submitted_chairperson_suggestion/index.js
@@ -1,5 +1,6 @@
 import { reqLog } from "@/services/api/logger";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
+import getSubmittedChairpersonSuggestionsHandler from "@/services/handlers/submitted_chairperson_suggestion/getSubmittedChairpersonSuggestionsHandler";
 import postSubmittedChairpersonSuggestion from "@/services/handlers/submitted_chairperson_suggestion/postSubmittedChairpersonSuggestion";
 
 export default async function handler(req, res) {
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       return postSubmittedChairpersonSuggestion(req, res);
+    case "GET":
+      return getSubmittedChairpersonSuggestionsHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
