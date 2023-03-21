49047d176f7a03416483491cc27afc816e97f58b
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-21 16:18:37 +0800

    implemented READ SubmittedPeerSuggestions

--- pages/api/submitted_peer_suggestion/index.js
+++ pages/api/submitted_peer_suggestion/index.js
@@ -1,5 +1,6 @@
 import { reqLog } from "@/services/api/logger";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
+import getSubmittedPeerSuggestionsHandler from "@/services/handlers/submitted_peer_suggestion/getSubmittedPeerSuggestionsHandler";
 import postSubmittedPeerSuggestionHandler from "@/services/handlers/submitted_peer_suggestion/postSubmittedPeerSuggestionHandler";
 
 export default async function handler(req, res) {
@@ -7,6 +8,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       return postSubmittedPeerSuggestionHandler(req, res);
+    case "GET":
+      return getSubmittedPeerSuggestionsHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
