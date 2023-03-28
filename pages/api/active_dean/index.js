b45b33146ae6328616e22c6665d8c97d7941d7e8
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-28 21:04:05 +0800

    implemented GET ActiveDeans

--- pages/api/active_dean/index.js
+++ pages/api/active_dean/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import getActiveDeansHandler from "@/services/handlers/active_dean/getActiveDeansHandler";
 import postActiveDeanHandler from "@/services/handlers/active_dean/postActiveDeanHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       return postActiveDeanHandler(req, res);
+    case "GET":
+      return getActiveDeansHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
