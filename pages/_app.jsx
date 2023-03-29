98b576620dd4cf3c1e4ca00fc4a223763dd013d1
Mark Zhang <68852130+archaga@users.noreply.github.com>
2023-03-29 09:46:52 +0800

    admin charts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,8 +3,34 @@ import theme from "@/theme";
 import CloseIcon from "@mui/icons-material/Close";
 import { IconButton } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
+import {
+  ArcElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Filler,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+} from "chart.js";
 import { SessionProvider } from "next-auth/react";
 import { closeSnackbar, SnackbarProvider } from "notistack";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Tooltip,
+  Title,
+  Legend,
+  RadialLinearScale,
+  Filler
+);
 
 export default function App({
   Component,
