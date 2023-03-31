26ec14cb3fd8bb57811cbd52ca0bfdcf40fec6ec
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-31 14:32:54 +0800

    implemented CITLDirector on admin panel

--- services/api/citl_director/readCITLDirectors.js
+++ services/api/citl_director/readCITLDirectors.js
@@ -1,14 +1,43 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import _ from "lodash";
 
-export default async function readCITLDirectors({ limit, page }) {
+export default async function readCITLDirectors({
+  limit,
+  page,
+  name,
+  sortColumn,
+  sortOrder,
+}) {
   const prisma = PRISMA_CLIENT;
+  const sortFilter = {};
+  _.set(sortFilter, sortColumn, sortOrder);
 
   try {
     const cITLDirectors = await prisma.cITLDirector.findMany({
       take: limit,
       skip: (page - 1) * limit,
+      include: {
+        User: true,
+        ActiveCITLDirector: true,
+      },
+      where: {
+        User: {
+          name: {
+            contains: name,
+          },
+        },
+      },
+      orderBy: sortFilter,
     });
-    const total = await prisma.cITLDirector.count();
+    const total = await prisma.cITLDirector.count({
+      where: {
+        User: {
+          name: {
+            contains: name,
+          },
+        },
+      },
+    });
     return { data: cITLDirectors, total };
   } catch (error) {
     throw error;
---
+++ views/admin/citl_director/AdminAddCITLDirectorForm.jsx
@@ -0,0 +1,53 @@
+import UserSelectField from "@/components/admin/form/UserSelectField";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Stack,
+} from "@mui/material";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+export default function AdminAddCITLDirectorForm({ open, onClose, onSubmit }) {
+  const formik = useFormik({
+    initialValues: {
+      userId: "",
+    },
+    validationSchema: Yup.object({
+      userId: Yup.string().required("User is required"),
+    }),
+    onSubmit: (values) => {
+      const { userId } = values;
+
+      onSubmit({ userId }).then((res) => {
+        onClose();
+      });
+    },
+  });
+
+  return (
+    <Dialog open={open} onClose={onClose} fullWidth>
+      <DialogTitle>Add CITL Director</DialogTitle>
+      <form noValidate onSubmit={formik.handleSubmit}>
+        <DialogContent>
+          <Stack spacing={1} sx={{ mt: 1 }}>
+            <UserSelectField
+              {...formik.getFieldProps("userId")}
+              onChange={(userId) => formik.setFieldValue("userId", userId)}
+              error={Boolean(formik.touched.userId && formik.errors.userId)}
+              helperText={formik.touched.userId && formik.errors.userId}
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button type='submit' disable={formik.isSubmitting}>
+            Submit
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+}
