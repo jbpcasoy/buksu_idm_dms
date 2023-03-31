94eba8f92423db23d5ca7353703ed55a09bc071b
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-31 10:29:16 +0800

    implemented DELETE ActiveIMDCoordinator

--- pages/api/active_imd_coordinator/[id]/index.js
+++ pages/api/active_imd_coordinator/[id]/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import deleteActiveIMDCoordinatorHandler from "@/services/handlers/active_imd_coordinator/deleteActiveIMDCoordinatorHandler";
 import getActiveIMDCoordinatorHandler from "@/services/handlers/active_imd_coordinator/getActiveIMDCoordinatorHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getActiveIMDCoordinatorHandler(req, res);
+    case "DELETE":
+      return deleteActiveIMDCoordinatorHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
