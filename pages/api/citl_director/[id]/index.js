8b0ab8b525a22def0658c5bcb4b9c75e8599dcf7
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-31 13:14:23 +0800

    implemented DELETE CITLDirector

--- pages/api/citl_director/[id]/index.js
+++ pages/api/citl_director/[id]/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import deleteCITLDirectorHandler from "@/services/handlers/citl_director/deleteCITLDirectorHandler";
 import getCITLDirectorHandler from "@/services/handlers/citl_director/getCITLDirectorHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
 
@@ -8,6 +9,8 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return getCITLDirectorHandler(req, res);
+    case "DELETE":
+      return deleteCITLDirectorHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
