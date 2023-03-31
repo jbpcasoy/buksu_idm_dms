d3d4d8e3896cfb3fc6b5952496fde010dc59465a
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-03-31 15:24:08 +0800

    fixed sidebar

---
+++ pages/home/index.jsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout/Layout";
+
+export default function index() {
+  return (
+    <Layout>
+      <h1 className='text-9xl text-center'>Welcome!</h1>
+    </Layout>
+  );
+}
