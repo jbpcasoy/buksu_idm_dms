7cc4a0430ff75c47a13e01229f7766015b74dc23
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-03 11:38:28 +0800

    implemented DELETE IMDCoordinatorSuggestionItem

--- pages/api/imd_coordinator_suggestion_item/[id]/index.js
+++ pages/api/imd_coordinator_suggestion_item/[id]/index.js
@@ -1,4 +1,5 @@
 import { reqLog } from "@/services/api/logger";
+import deleteIMDCoordinatorSuggestionItemHandler from "@/services/handlers/imd_coordinator_suggestion_item/deleteIMDCoordinatorSuggestionItemHandler";
 import getIMDCoordinatorSuggestionItemHandler from "@/services/handlers/imd_coordinator_suggestion_item/getIMDCoordinatorSuggestionItemHandler";
 import putIMDCoordinatorSuggestionItemHandler from "@/services/handlers/imd_coordinator_suggestion_item/putIMDCoordinatorSuggestionItemHandler";
 import methodNaHandler from "@/services/handlers/methodNaHandler";
@@ -10,6 +11,8 @@ export default async function handler(req, res) {
       return getIMDCoordinatorSuggestionItemHandler(req, res);
     case "PUT":
       return putIMDCoordinatorSuggestionItemHandler(req, res);
+    case "DELETE":
+      return deleteIMDCoordinatorSuggestionItemHandler(req, res);
     default:
       return methodNaHandler(req, res);
   }
