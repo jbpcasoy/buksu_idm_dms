a8a9878ea9f13d8fe5fe0bde88e62344e5ff0806
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-07 12:29:07 +0800

    implemented IM new statuses on tracking

--- components/im_event/IMEvent.jsx
+++ components/im_event/IMEvent.jsx
@@ -1,14 +1,19 @@
 import useIMEvent from "@/hooks/im_event/useIMEvent";
 import IMCITLDirectorEndorsementEvent from "./IMCITLDirectorEndorsementEvent";
+import IMCITLReviewedEvent from "./IMCITLReviewedEvent";
+import IMCITLRevisedEvent from "./IMCITLRevisedEvent";
 import IMCoordinatorEndorsementEvent from "./IMCoordinatorEndorsementEvent";
 import IMCreatedEvent from "./IMCreatedEvent";
 import IMDeanEndorsementEvent from "./IMDeanEndorsementEvent";
+import IMDepartmentReviewedEvent from "./IMDepartmentReviewedEvent";
+import IMDepartmentRevisedEvent from "./IMDepartmentRevisedEvent";
 import IMIMDCoordinatorEndorsementEvent from "./IMIMDCoordinatorEndorsementEvent";
 import IMNewVersionEvent from "./IMNewVersionEvent";
 import IMSubmittedChairpersonReviewEvent from "./IMSubmittedChairpersonReviewEvent";
 import IMSubmittedChairpersonSuggestionEvent from "./IMSubmittedChairpersonSuggestionEvent";
 import IMSubmittedCoordinatorReviewEvent from "./IMSubmittedCoordinatorReviewEvent";
 import IMSubmittedCoordinatorSuggestionEvent from "./IMSubmittedCoordinatorSuggestionEvent";
+import IMSubmittedEvent from "./IMSubmittedEvent";
 import IMSubmittedIMDCoordinatorSuggestionEvent from "./IMSubmittedIMDCoordinatorSuggestionEvent";
 import IMSubmittedPeerReviewEvent from "./IMSubmittedPeerReviewEvent";
 import IMSubmittedPeerSuggestionEvent from "./IMSubmittedPeerSuggestionEvent";
@@ -19,6 +24,8 @@ export default function IMEvent({ iMEventId }) {
     return <IMCreatedEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "NEW_VERSION") {
     return <IMNewVersionEvent iMEvent={iMEvent} />;
+  } else if (iMEvent?.IMEventType === "SUBMITTED") {
+    return <IMSubmittedEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "SUBMITTED_PEER_REVIEW") {
     return <IMSubmittedPeerReviewEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "SUBMITTED_COORDINATOR_REVIEW") {
@@ -31,12 +38,20 @@ export default function IMEvent({ iMEventId }) {
     return <IMSubmittedCoordinatorSuggestionEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "SUBMITTED_CHAIRPERSON_SUGGESTION") {
     return <IMSubmittedChairpersonSuggestionEvent iMEvent={iMEvent} />;
+  } else if (iMEvent?.IMEventType === "DEPARTMENT_REVIEWED") {
+    return <IMDepartmentReviewedEvent iMEvent={iMEvent} />;
+  } else if (iMEvent?.IMEventType === "DEPARTMENT_REVISED") {
+    return <IMDepartmentRevisedEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "COORDINATOR_ENDORSEMENT") {
     return <IMCoordinatorEndorsementEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "DEAN_ENDORSEMENT") {
     return <IMDeanEndorsementEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "SUBMITTED_IMD_COORDINATOR_SUGGESTION") {
     return <IMSubmittedIMDCoordinatorSuggestionEvent iMEvent={iMEvent} />;
+  } else if (iMEvent?.IMEventType === "CITL_REVIEWED") {
+    return <IMCITLReviewedEvent iMEvent={iMEvent} />;
+  } else if (iMEvent?.IMEventType === "CITL_REVISED") {
+    return <IMCITLRevisedEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "IMD_COORDINATOR_ENDORSEMENT") {
     return <IMIMDCoordinatorEndorsementEvent iMEvent={iMEvent} />;
   } else if (iMEvent?.IMEventType === "CITL_DIRECTOR_ENDORSEMENT") {
