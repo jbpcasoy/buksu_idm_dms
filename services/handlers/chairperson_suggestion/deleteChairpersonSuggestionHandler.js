8c5fa3d726ee76288b4e6dd9f511ce3ecd226655
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-18 14:08:11 +0800

    secured delete chairperson suggestion

--- services/handlers/chairperson_suggestion/deleteChairpersonSuggestionHandler.js
+++ services/handlers/chairperson_suggestion/deleteChairpersonSuggestionHandler.js
@@ -1,8 +1,31 @@
+import userAbility from "@/services/abilities/defineAbility";
 import deleteChairpersonSuggestion from "@/services/api/chairperson_suggestion/deleteChairpersonSuggestion";
+import readChairpersonSuggestion from "@/services/api/chairperson_suggestion/readChairpersonSuggestion";
+import getServerUser from "@/services/helpers/getServerUser";
+import abilityValidator from "@/services/validator/abilityValidator";
 
 export default async function deleteChairpersonSuggestionHandler(req, res) {
   const { id } = req.query;
+  const user = await getServerUser(req, res);
 
-  const chairpersonSuggestion = await deleteChairpersonSuggestion(id);
-  return res.status(200).json(chairpersonSuggestion);
+  async function findSubject({ id }) {
+    const subject = await readChairpersonSuggestion({
+      id,
+      ability: await userAbility(user),
+    });
+    return subject;
+  }
+
+  return abilityValidator({
+    req,
+    res,
+    next: async (req, res) => {
+      const chairpersonSuggestion = await deleteChairpersonSuggestion(id);
+      return res.status(200).json(chairpersonSuggestion);
+    },
+    action: "delete",
+    subject: await findSubject({ id }),
+    fields: undefined,
+    type: "ChairpersonSuggestion",
+  });
 }
