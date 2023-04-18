673283286b2bf9326dfa069a421d0493c9a7a4b5
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-18 09:14:59 +0800

    secured delete chairperson review item

--- services/middleware/catchAllError.js
+++ services/middleware/catchAllError.js
@@ -1,4 +1,6 @@
 import handler from "@/pages/api/active_chairperson";
+import formatPrismaError from "../helpers/formatPrismaError";
+import { Prisma } from "@prisma/client";
 
 export default async function catchAllError(req, res, next) {
   try {
@@ -6,21 +8,14 @@ export default async function catchAllError(req, res, next) {
     return result;
   } catch (error) {
     console.error(error);
-    return res
-      .status(
-        error?.code === "P2025"
-          ? 404
-          : error?.code === "P2014" || error?.code === "P2002"
-          ? 400
-          : error?.statusCode ?? 500
-      )
-      .json({
-        message:
-          error?.code === "P2014"
-            ? `The change you are trying to make would violate the required relation '${error.meta.relation_name}' between the ${error.meta.model_a_name} and ${error.meta.model_b_name} models.`
-            : error?.code === "P2002"
-            ? `Unique constraint failed on the ${error.meta.target}`
-            : error?.message ?? "Something went wrong",
-      });
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      const { statusCode, message } = formatPrismaError(error);
+      return res.status(statusCode).json({ error: message });
+    }
+
+    return res.status(error?.statusCode ?? 500).json({
+      message: error?.message ?? "Something went wrong",
+    });
   }
 }
