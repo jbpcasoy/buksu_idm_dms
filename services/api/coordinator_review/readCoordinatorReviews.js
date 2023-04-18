171e5dc1b1cb9eca0152404b71d01f17ce15e1ca
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-18 23:13:53 +0800

    secured get coordinator reviews

--- services/api/coordinator_review/readCoordinatorReviews.js
+++ services/api/coordinator_review/readCoordinatorReviews.js
@@ -1,29 +1,42 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
 export default async function readCoordinatorReviews({
   limit,
   page,
   iMId,
   coordinatorId,
+  ability,
 }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = await accessibleBy(ability).CoordinatorReview;
 
   const coordinatorReviews = await prisma.coordinatorReview.findMany({
     take: limit,
     skip: (page - 1) * limit,
     where: {
-      coordinatorId: {
-        contains: coordinatorId,
-      },
-      iMId: { contains: iMId },
+      AND: [
+        accessibility,
+        {
+          coordinatorId: {
+            contains: coordinatorId,
+          },
+          iMId: { contains: iMId },
+        },
+      ],
     },
   });
   const total = await prisma.coordinatorReview.count({
     where: {
-      coordinatorId: {
-        contains: coordinatorId,
-      },
-      iMId: { contains: iMId },
+      AND: [
+        accessibility,
+        {
+          coordinatorId: {
+            contains: coordinatorId,
+          },
+          iMId: { contains: iMId },
+        },
+      ],
     },
   });
 
