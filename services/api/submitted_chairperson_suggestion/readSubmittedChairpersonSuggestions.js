228dab31c5c07738eaf354b80006a5831042e7f9
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-18 15:33:39 +0800

    secured delete submitted chairperson suggestions

--- services/api/submitted_chairperson_suggestion/readSubmittedChairpersonSuggestions.js
+++ services/api/submitted_chairperson_suggestion/readSubmittedChairpersonSuggestions.js
@@ -1,16 +1,27 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
 export default async function readSubmittedChairpersonSuggestions({
   limit,
   page,
+  ability,
 }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = await accessibleBy(ability)
+    .SubmittedChairpersonSuggestion;
 
   const submittedChairpersonSuggestion =
     await prisma.submittedChairpersonSuggestion.findMany({
       take: limit,
       skip: (page - 1) * limit,
+      where: {
+        AND: [accessibility],
+      },
     });
-  const total = await prisma.submittedChairpersonSuggestion.count();
+  const total = await prisma.submittedChairpersonSuggestion.count({
+    where: {
+      AND: [accessibility],
+    },
+  });
   return { data: submittedChairpersonSuggestion, total };
 }
