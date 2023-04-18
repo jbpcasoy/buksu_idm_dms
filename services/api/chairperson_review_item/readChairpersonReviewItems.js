2fedd68c55cf6df1ee023666eea34e963793e15e
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-18 11:08:00 +0800

    secured get chairperson review items

--- services/api/chairperson_review_item/readChairpersonReviewItems.js
+++ services/api/chairperson_review_item/readChairpersonReviewItems.js
@@ -1,25 +1,38 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
 export default async function readChairpersonReviewItems({
   limit,
   page,
   questionId,
   chairpersonReviewId,
+  ability,
 }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).ChairpersonReviewItem;
 
   const chairpersonReviewItems = await prisma.chairpersonReviewItem.findMany({
     take: limit,
     skip: limit && page ? (page - 1) * limit : undefined,
     where: {
-      questionId,
-      chairpersonReviewId,
+      AND: [
+        accessibility,
+        {
+          questionId,
+          chairpersonReviewId,
+        },
+      ],
     },
   });
   const total = await prisma.chairpersonReviewItem.count({
     where: {
-      questionId,
-      chairpersonReviewId,
+      AND: [
+        accessibility,
+        {
+          questionId,
+          chairpersonReviewId,
+        },
+      ],
     },
   });
   return { data: chairpersonReviewItems, total };
