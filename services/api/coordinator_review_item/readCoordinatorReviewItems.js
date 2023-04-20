9e882650e6dcfbfd436345a20d053b4541e5e32f
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-20 16:41:08 +0800

    secured ger coordinator review items

--- services/api/coordinator_review_item/readCoordinatorReviewItems.js
+++ services/api/coordinator_review_item/readCoordinatorReviewItems.js
@@ -1,29 +1,42 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
 export default async function readCoordinatorReviewItems({
   limit,
   page,
   questionId,
   coordinatorReviewId,
+  ability,
 }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).CoordinatorReviewItem;
 
   const coordinatorReviewItems = await prisma.coordinatorReviewItem.findMany({
     take: limit,
     skip: limit && page ? (page - 1) * limit : undefined,
     where: {
-      questionId: {
-        contains: questionId,
-      },
-      coordinatorReviewId: { contains: coordinatorReviewId },
+      AND: [
+        accessibility,
+        {
+          questionId: {
+            contains: questionId,
+          },
+          coordinatorReviewId: { contains: coordinatorReviewId },
+        },
+      ],
     },
   });
   const total = await prisma.coordinatorReviewItem.count({
     where: {
-      questionId: {
-        contains: questionId,
-      },
-      coordinatorReviewId: { contains: coordinatorReviewId },
+      AND: [
+        accessibility,
+        {
+          questionId: {
+            contains: questionId,
+          },
+          coordinatorReviewId: { contains: coordinatorReviewId },
+        },
+      ],
     },
   });
 
