9b6a6d466a3f80ca51d42c2ab69b3fbf18a25f24
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-24 18:10:54 +0800

    secured get submitted peer suggestions

--- services/api/submitted_peer_suggestion/readSubmittedPeerSuggestions.js
+++ services/api/submitted_peer_suggestion/readSubmittedPeerSuggestions.js
@@ -1,13 +1,26 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
-export default async function readSubmittedPeerSuggestions({ limit, page }) {
+export default async function readSubmittedPeerSuggestions({
+  limit,
+  page,
+  ability,
+}) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).SubmittedPeerSuggestion;
 
   const submittedPeerSuggestions =
     await prisma.submittedPeerSuggestion.findMany({
       take: limit,
       skip: (page - 1) * limit,
+      where: {
+        AND: [accessibility],
+      },
     });
-  const total = await prisma.submittedPeerSuggestion.count();
+  const total = await prisma.submittedPeerSuggestion.count({
+    where: {
+      AND: [accessibility],
+    },
+  });
   return { data: submittedPeerSuggestions, total };
 }
--- services/handlers/submitted_peer_suggestion/getSubmittedPeerSuggestionsHandler.js
+++ services/handlers/submitted_peer_suggestion/getSubmittedPeerSuggestionsHandler.js
@@ -1,10 +1,15 @@
+import userAbility from "@/services/abilities/defineAbility";
 import readSubmittedPeerSuggestions from "@/services/api/submitted_peer_suggestion/readSubmittedPeerSuggestions";
+import getServerUser from "@/services/helpers/getServerUser";
 
 export default async function getSubmittedPeerSuggestionsHandler(req, res) {
   const { limit, page } = req.query;
+  const user = await getServerUser(req, res);
+
   const submittedPeerSuggestions = await readSubmittedPeerSuggestions({
     limit: parseInt(limit),
     page: parseInt(page),
+    ability: await userAbility(user),
   });
 
   return res.status(200).json(submittedPeerSuggestions);
