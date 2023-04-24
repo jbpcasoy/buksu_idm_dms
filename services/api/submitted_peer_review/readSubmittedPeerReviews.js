f400803819d9b0b0f9534aac7b02e666554b39ae
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-24 15:17:34 +0800

    secured get submitted peer reviews

--- services/api/submitted_peer_review/readSubmittedPeerReviews.js
+++ services/api/submitted_peer_review/readSubmittedPeerReviews.js
@@ -1,27 +1,40 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
 export default async function readSubmittedPeerReviews({
   limit,
   page,
   peerReviewId,
+  ability,
 }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).SubmittedPeerReview;
 
   const submittedPeerReviews = await prisma.submittedPeerReview.findMany({
     take: limit,
     skip: limit && page ? (page - 1) * limit : undefined,
     where: {
-      peerReviewId: {
-        contains: peerReviewId,
-      },
+      AND: [
+        accessibility,
+        {
+          peerReviewId: {
+            contains: peerReviewId,
+          },
+        },
+      ],
     },
   });
 
   const total = await prisma.submittedPeerReview.count({
     where: {
-      peerReviewId: {
-        contains: peerReviewId,
-      },
+      AND: [
+        accessibility,
+        {
+          peerReviewId: {
+            contains: peerReviewId,
+          },
+        },
+      ],
     },
   });
 
--- services/handlers/submitted_peer_review/getSubmittedPeerReviewsHandler.js
+++ services/handlers/submitted_peer_review/getSubmittedPeerReviewsHandler.js
@@ -1,12 +1,16 @@
+import userAbility from "@/services/abilities/defineAbility";
 import readSubmittedPeerReviews from "@/services/api/submitted_peer_review/readSubmittedPeerReviews";
+import getServerUser from "@/services/helpers/getServerUser";
 
 export default async function getSubmittedPeerReviewsHandler(req, res) {
   const { limit, page, peerReviewId } = req.query;
+  const user = await getServerUser(req, res);
 
   const submittedPeerReviews = await readSubmittedPeerReviews({
     limit: limit ? parseInt(limit) : undefined,
     page: page ? parseInt(page) : undefined,
     peerReviewId,
+    ability: await userAbility(user),
   });
   return res.status(200).json(submittedPeerReviews);
 }
