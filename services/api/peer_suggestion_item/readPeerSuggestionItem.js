4c99dccbbb6d5178a4cc7714272e4b7de84f5876
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-24 16:22:18 +0800

    secured get peer suggestion item

--- services/api/peer_suggestion_item/readPeerSuggestionItem.js
+++ services/api/peer_suggestion_item/readPeerSuggestionItem.js
@@ -1,11 +1,18 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
-export default async function readPeerSuggestionItem(id) {
+export default async function readPeerSuggestionItem({ id, ability }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).PeerSuggestionItem;
 
-  const peerSuggestionItem = await prisma.peerSuggestionItem.findUniqueOrThrow({
+  const peerSuggestionItem = await prisma.peerSuggestionItem.findFirstOrThrow({
     where: {
-      id,
+      AND: [
+        accessibility,
+        {
+          id,
+        },
+      ],
     },
   });
   return peerSuggestionItem;
