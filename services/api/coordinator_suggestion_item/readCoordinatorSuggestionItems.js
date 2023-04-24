dffc93129dd4bb17eea76ce47e9a0cde6b948cc8
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-24 10:24:42 +0800

    secured get coordinator suggestion items

--- services/api/coordinator_suggestion_item/readCoordinatorSuggestionItems.js
+++ services/api/coordinator_suggestion_item/readCoordinatorSuggestionItems.js
@@ -1,27 +1,40 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
 export default async function readCoordinatorSuggestionItems({
   limit,
   page,
   coordinatorSuggestionId,
+  ability,
 }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).CoordinatorSuggestionItem;
 
   const coordinatorSuggestionItems =
     await prisma.coordinatorSuggestionItem.findMany({
       take: limit,
       skip: limit && page ? (page - 1) * limit : undefined,
       where: {
-        coordinatorSuggestionId: {
-          contains: coordinatorSuggestionId,
-        },
+        AND: [
+          accessibility,
+          {
+            coordinatorSuggestionId: {
+              contains: coordinatorSuggestionId,
+            },
+          },
+        ],
       },
     });
   const total = await prisma.coordinatorSuggestionItem.count({
     where: {
-      coordinatorSuggestionId: {
-        contains: coordinatorSuggestionId,
-      },
+      AND: [
+        accessibility,
+        {
+          coordinatorSuggestionId: {
+            contains: coordinatorSuggestionId,
+          },
+        },
+      ],
     },
   });
   return { data: coordinatorSuggestionItems, total };
--- services/handlers/coordinator_suggestion_item/getCoordinatorSuggestionItemsHandler.js
+++ services/handlers/coordinator_suggestion_item/getCoordinatorSuggestionItemsHandler.js
@@ -1,12 +1,16 @@
+import userAbility from "@/services/abilities/defineAbility";
 import readCoordinatorSuggestionItems from "@/services/api/coordinator_suggestion_item/readCoordinatorSuggestionItems";
+import getServerUser from "@/services/helpers/getServerUser";
 
 export default async function getCoordinatorSuggestionItemsHandler(req, res) {
   const { limit, page, coordinatorSuggestionId } = req.query;
+  const user = await getServerUser(req, res);
 
   const coordinatorSuggestionItems = await readCoordinatorSuggestionItems({
     limit: limit ? parseInt(limit) : undefined,
     page: page ? parseInt(page) : undefined,
     coordinatorSuggestionId,
+    ability: await userAbility(user),
   });
   return res.status(200).json(coordinatorSuggestionItems);
 }
