c52f17f109f6b3f400cca8a491eed6fb17779822
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-24 11:55:23 +0800

    secured get peer reviews

--- services/api/peer_review/readPeerReviews.js
+++ services/api/peer_review/readPeerReviews.js
@@ -1,34 +1,47 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
 export default async function readPeerReviews({
   limit,
   page,
   facultyId,
   iMId,
+  ability,
 }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).PeerReview;
 
   const peerReviews = await prisma.peerReview.findMany({
     take: limit,
     skip: (page - 1) * limit,
     where: {
-      facultyId: {
-        contains: facultyId,
-      },
-      iMId: {
-        contains: iMId,
-      },
+      AND: [
+        accessibility,
+        {
+          facultyId: {
+            contains: facultyId,
+          },
+          iMId: {
+            contains: iMId,
+          },
+        },
+      ],
     },
   });
 
   const total = await prisma.peerReview.count({
     where: {
-      facultyId: {
-        contains: facultyId,
-      },
-      iMId: {
-        contains: iMId,
-      },
+      AND: [
+        accessibility,
+        {
+          facultyId: {
+            contains: facultyId,
+          },
+          iMId: {
+            contains: iMId,
+          },
+        },
+      ],
     },
   });
   return { data: peerReviews, total };
--- services/handlers/peer_review/getPeerReviewsHandler.js
+++ services/handlers/peer_review/getPeerReviewsHandler.js
@@ -1,13 +1,17 @@
+import userAbility from "@/services/abilities/defineAbility";
 import readPeerReviews from "@/services/api/peer_review/readPeerReviews";
+import getServerUser from "@/services/helpers/getServerUser";
 
 export default async function getPeerReviewsHandler(req, res) {
   const { limit = 10, page = 1, facultyId, iMId } = req.query;
+  const user = await getServerUser(req, res);
 
   const peerReviews = await readPeerReviews({
     limit: parseInt(limit),
     page: parseInt(page),
     facultyId,
     iMId,
+    ability: await userAbility(user),
   });
   return res.status(200).json(peerReviews);
 }
