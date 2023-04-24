6e1b81d69c300209133a00803e50ca09adf727df
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-24 15:04:13 +0800

    secured get submitted peer review

--- services/api/submitted_peer_review/readSubmittedPeerReview.js
+++ services/api/submitted_peer_review/readSubmittedPeerReview.js
@@ -1,13 +1,26 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
-export default async function readSubmittedPeerReview(id) {
+export default async function readSubmittedPeerReview({
+  id,
+  ability,
+  filter = {},
+}) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).SubmittedPeerReview;
 
-  const submittedPeerReview =
-    await prisma.submittedPeerReview.findUniqueOrThrow({
+  const submittedPeerReview = await prisma.submittedPeerReview.findFirstOrThrow(
+    {
       where: {
-        id,
+        AND: [
+          accessibility,
+          {
+            ...filter,
+            id,
+          },
+        ],
       },
-    });
+    }
+  );
   return submittedPeerReview;
 }
