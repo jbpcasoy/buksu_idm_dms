e1acfe60f683afc5dc078ecaca8c50d163fce6d3
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-24 13:58:43 +0800

    secured get submitted coordinator suggestion

--- services/api/submitted_coordinator_suggestion/readSubmittedCoordinatorSuggestion.js
+++ services/api/submitted_coordinator_suggestion/readSubmittedCoordinatorSuggestion.js
@@ -1,12 +1,24 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
-export default async function readSubmittedCoordinatorSuggestion(id) {
+export default async function readSubmittedCoordinatorSuggestion({
+  id,
+  ability,
+  filter = {},
+}) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).SubmittedCoordinatorSuggestion;
 
   const submittedCoordinatorSuggestion =
-    await prisma.submittedCoordinatorSuggestion.findUniqueOrThrow({
+    await prisma.submittedCoordinatorSuggestion.findFirstOrThrow({
       where: {
-        id,
+        AND: [
+          accessibility,
+          {
+            ...filter,
+            id,
+          },
+        ],
       },
     });
   return submittedCoordinatorSuggestion;
