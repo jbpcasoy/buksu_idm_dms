d04155694883f8bcf6da1a25f83aa6d72967331f
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-25 14:18:36 +0800

    secured get imd coordinator endorsements

--- services/api/imd_coordinator_endorsement/readIMDCoordinatorEndorsements.js
+++ services/api/imd_coordinator_endorsement/readIMDCoordinatorEndorsements.js
@@ -1,14 +1,27 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
-export default async function readIMDCoordinatorEndorsements({ limit, page }) {
+export default async function readIMDCoordinatorEndorsements({
+  limit,
+  page,
+  ability,
+}) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).IMDCoordinatorEndorsement;
 
   const iMDCoordinatorEndorsements =
     await prisma.iMDCoordinatorEndorsement.findMany({
       take: limit,
       skip: (page - 1) * limit,
+      where: {
+        AND: [accessibility],
+      },
     });
 
-  const total = await prisma.iMDCoordinatorEndorsement.count();
+  const total = await prisma.iMDCoordinatorEndorsement.count({
+    where: {
+      AND: [accessibility],
+    },
+  });
   return { data: iMDCoordinatorEndorsements, total };
 }
