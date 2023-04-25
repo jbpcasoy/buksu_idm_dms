38901aaeb220590eae512eb340ecbdaa4e01a9dd
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-25 16:23:51 +0800

    secured delete file

--- services/handlers/chairperson_review/deleteChairpersonReviewHandler.js
+++ services/handlers/chairperson_review/deleteChairpersonReviewHandler.js
@@ -2,7 +2,6 @@ import userAbility from "@/services/abilities/defineAbility";
 import deleteChairpersonReview from "@/services/api/chairperson_review/deleteChairpersonReview";
 import readChairpersonReview from "@/services/api/chairperson_review/readChairpersonReview";
 import getServerUser from "@/services/helpers/getServerUser";
-import statusError from "@/services/helpers/throwError";
 import abilityValidator from "@/services/validator/abilityValidator";
 
 export default async function deleteChairpersonReviewHandler(req, res) {
