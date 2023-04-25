d1e60de2d3492c3c907b9b4fde86ce22f3b541a1
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-04-25 10:32:09 +0800

    secured get imd coordinator suggestions

--- services/api/imd_coordinator_suggestion/readIMDCoordinatorSuggestions.js
+++ services/api/imd_coordinator_suggestion/readIMDCoordinatorSuggestions.js
@@ -1,23 +1,36 @@
 import { PRISMA_CLIENT } from "@/prisma/prisma_client";
+import { accessibleBy } from "@casl/prisma";
 
 export default async function readIMDCoordinatorSuggestions({
   limit,
   page,
   iMId,
+  ability,
 }) {
   const prisma = PRISMA_CLIENT;
+  const accessibility = accessibleBy(ability).IMDCoordinatorSuggestion;
 
   const iMDCoordinatorSuggestions =
     await prisma.iMDCoordinatorSuggestion.findMany({
       take: limit,
       skip: page && limit ? (page - 1) * limit : undefined,
       where: {
-        iMId,
+        AND: [
+          accessibility,
+          {
+            iMId,
+          },
+        ],
       },
     });
   const total = await prisma.iMDCoordinatorSuggestion.count({
     where: {
-      iMId,
+      AND: [
+        accessibility,
+        {
+          iMId,
+        },
+      ],
     },
   });
 
