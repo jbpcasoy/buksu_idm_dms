d34d2c3fce34ed65b1b31def0e463f60a5dcd3bd
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-05-03 04:13:26 +0800

    fixed print button color

--- components/pdf/print/IMReviewPrintDialog.jsx
+++ components/pdf/print/IMReviewPrintDialog.jsx
@@ -33,7 +33,7 @@ export default function IMReviewPrintDialog({
       <button
         onClick={() => setShow((prev) => ({ ...prev, openPrint: true }))}
         type='button'
-        class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+        class='text-CITLDarkBlue bg-white hover:bg-CITLDarkBlue hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
       >
         <svg
           xmlns='http://www.w3.org/2000/svg'
