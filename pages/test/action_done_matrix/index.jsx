cb2cfd074faf987a8b34dbcb63aa48db169da0b4
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-05-04 13:05:39 +0800

    implemented adm printing page

---
+++ pages/test/action_done_matrix/index.jsx
@@ -0,0 +1,6 @@
+import ActionDoneMatrix from "@/components/pdf/action_done_matrix/ActionDoneMatrix";
+import React from "react";
+
+export default function index() {
+  return <ActionDoneMatrix />;
+}
