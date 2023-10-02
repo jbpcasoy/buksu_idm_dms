9ca9851d4c1c4befdbe3ef422512ad145fec327e
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-10-02 09:52:10 +0800

    changed prisma client handling

--- prisma/prisma_client.js
+++ prisma/prisma_client.js
@@ -1,6 +1,7 @@
+const { default: client } = require("@/lib/prismadb");
 const { PrismaClient } = require("@prisma/client");
 
-const PRISMA_CLIENT = new PrismaClient();
+const PRISMA_CLIENT = client;
 module.exports = {
   PRISMA_CLIENT: PRISMA_CLIENT,
 };
